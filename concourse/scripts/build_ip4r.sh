aa2791c620c783659215f7f4f712b2851fda68b9
liuxueyang <liuxueyang457@gmail.com>
2022-07-12 17:18:29 +0800

    modify pkg function

--- concourse/scripts/build_ip4r.sh
+++ concourse/scripts/build_ip4r.sh
@@ -2,6 +2,10 @@
 
 set -exo pipefail
 
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOP_DIR=${CWDIR}/../../../
+
+source "${TOP_DIR}/gpdb_src/concourse/scripts/common.bash"
 function pkg() {
     [ -f /opt/gcc_env.sh ] && source /opt/gcc_env.sh
     source /usr/local/greenplum-db-devel/greenplum_path.sh
@@ -15,6 +19,22 @@ function pkg() {
     # last_release_path=$(readlink -e /home/gpadmin/last_released_ip4r_bin/ip4r-*.tar.gz)
     make clean
     make install
+    popd
+
+    pushd /usr/local/greenplum-db-devel/
+    echo 'cp -r lib share $GPHOME || exit 1'> install_gpdb_component
+    chmod a+x install_gpdb_component
+    tar -czf "$TOP_DIR/bin_ip4r/ip4r_x86_64.tar.gz" \
+        "lib/postgresql/ip4r.so" \
+        "share/postgresql/extension/ip4r.control" \
+        "share/postgresql/extension/ip4r--2.4.sql" \
+        "share/postgresql/extension/ip4r--2.2--2.4.sql" \
+        "share/postgresql/extension/ip4r--2.1--2.2.sql" \
+        "share/postgresql/extension/ip4r--2.0--2.1.sql" \
+        "share/postgresql/extension/ip4r--unpackaged2.1--2.1.sql" \
+        "share/postgresql/extension/ip4r--unpackaged2.0--2.0.sql" \
+        "share/postgresql/extension/ip4r--unpackaged1--2.0.sql" \
+        "install_gpdb_component"
     popd
 }
 
