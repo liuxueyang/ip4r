af85a2379116cc46e84a29565898984b9a1fc8c9
liuxueyang <liuxueyang457@gmail.com>
2022-07-12 15:53:40 +0800

    change build_ip4r.sh

--- concourse/scripts/build_ip4r.sh
+++ concourse/scripts/build_ip4r.sh
@@ -10,8 +10,8 @@ function pkg() {
         export CC="$(which gcc)"
     fi
 
-    pushd /home/gpadmin/ip4r_artifacts
-    local last_release_path
+    pushd /home/gpadmin/ip4r_src
+    # local last_release_path
     # last_release_path=$(readlink -e /home/gpadmin/last_released_ip4r_bin/ip4r-*.tar.gz)
     make clean
     make install
